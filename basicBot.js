5d684304d0b4a4b8e38347ff72138ca80d3f0f65
aalinnfs <aalinnfs@gmail.com>
2015-08-01 22:59:25 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1911,7 +1911,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat(subChat(basicBot.chat.commandslink, {botname: basicBot.settings.botName, link: basicBot.cmdLink}));
+                              API.sendChat("/me Karl Bot commands: https://goo.gl/oX3C3r");
                     }
                 }
             },
