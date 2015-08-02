225ddf9b4e9c18d6724e6a2d28318730e6325b97
aalinnfs <aalinnfs@gmail.com>
2015-08-03 01:51:59 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -237,7 +237,7 @@
     var botCreatorIDs = ["4856169", "5596573"];
 
     var basicBot = {
-        version: "3.1",
+        version: "3.2",
         status: false,
         name: "Karl Bot",
         loggedInID: null,
