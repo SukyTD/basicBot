c8ccd808ee783a3839d725a37efb9d83f048fb91
aalinnfs <aalinnfs@gmail.com>
2015-08-10 15:36:52 +0300

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -22,7 +22,35 @@ var eventCountdownMessages = (function(running) {
                         }, (startTime * 60000) - 10000);
                         setTimeout(function(){
                                 API.sendChat("9 seconds until the fest starts!");
-                        }, (startTime * 60000) - 11000);
+                        }, (startTime * 60000) - 9000);
+                        setTimeout(function(){
+                                API.sendChat("8 seconds until the fest starts!");
+                        }, (startTime * 60000) - 8000);
+                        setTimeout(function(){
+                                API.sendChat("7 seconds until the fest starts!");
+                        }, (startTime * 60000) - 7000);
+                        setTimeout(function(){
+                                API.sendChat("6 seconds until the fest starts!");
+                        }, (startTime * 60000) - 6000);
+                        setTimeout(function(){
+                                API.sendChat("5 seconds until the fest starts!");
+                        }, (startTime * 60000) - 5000);
+                        setTimeout(function(){
+                                API.sendChat("4 seconds until the fest starts!");
+                        }, (startTime * 60000) - 4000);
+                        setTimeout(function(){
+                                API.sendChat("3 seconds until the fest starts!");
+                        }, (startTime * 60000) - 3000);
+                        setTimeout(function(){
+                                API.sendChat("2 seconds until the fest starts!");
+                        }, (startTime * 60000) - 2000);
+                        setTimeout(function(){
+                                API.sendChat("1 seconds until the fest starts!");
+                        }, (startTime * 60000) - 1000);
+                        setTimeout(function(){
+                                API.sendChat("The fest started! Welcome to the nation!");
+                        }, (startTime * 60000) - 0.1);
+                        
                 };
         });
 })(eventCountdownMessages);
