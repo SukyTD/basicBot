ff471d362709ae6f0a5ed47b3493086fafbfa8c1
aalinnfs <aalinnfs@gmail.com>
2015-08-12 18:55:33 +0300

    Changed the dclookupwelcome
    So now it should put you in the spot you was, not in the spot you was - the songs passed

--- basicBot.js
+++ basicBot.js
@@ -237,7 +237,7 @@
     var botCreatorIDs = ["4856169", "5596573"];
 
     var basicBot = {
-        version: "3.3.3",
+        version: "3.3.4",
         status: false,
         name: "Karl Bot",
         loggedInID: null,
