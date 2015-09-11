2b1d62959b47a69dd6c49fec1abf4d0e29448618
Alin Marian Lupu <aalinnfs@gmail.com>
2015-09-11 15:42:11 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3638,8 +3638,8 @@
                         */
 
                         // This is a more efficient solution
-                        if (msg.length > 238){
-                            var split = msg.match(/.{1,238}/g);
+                        if (msg.length > 241){
+                            var split = msg.match(/.{1,241}/g);
                             for (var i = 0; i < split.length; i++) {
                                 var func = function(index) {
                                     setTimeout(function() {
